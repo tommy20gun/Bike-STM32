efdaf18f8c23220a6ac7f8e8595f061b601b9002
tommy20gun <tommy20gun00@gmail.com>
2025-02-07 19:00:49 -0800

    Generated RTOS HAL Code to "necessary files only", Cleanup Makefile, Moved Workspace

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -27,8 +27,18 @@ extern "C" {
 #endif
 
 /* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
 
-
+#include "stm32f4xx_ll_rcc.h"
+#include "stm32f4xx_ll_bus.h"
+#include "stm32f4xx_ll_system.h"
+#include "stm32f4xx_ll_exti.h"
+#include "stm32f4xx_ll_cortex.h"
+#include "stm32f4xx_ll_utils.h"
+#include "stm32f4xx_ll_pwr.h"
+#include "stm32f4xx_ll_dma.h"
+#include "stm32f4xx_ll_usart.h"
+#include "stm32f4xx_ll_gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -37,7 +47,6 @@ extern "C" {
 #endif /* USE_FULL_ASSERT */
 #include <Peripheral_initialization.h>
 #include <cmsis_os.h>
-#include "stm32f4xx_ll_gpio.h"
 /*
 #include "stm32f4xx_ll_rcc.h"
 #include "stm32f4xx_ll_bus.h"
--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -45,3 +45,13 @@ int main(){
   osKernelStart();
 }
 
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
