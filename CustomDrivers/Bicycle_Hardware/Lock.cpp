513f7405b55b3313c5693b9c679b152512268642
Tommy <tommy20gun00@gmail.com>
2025-05-02 14:00:27 -0700

    small change for prescaler

--- CustomDrivers/Bicycle_Hardware/Lock.cpp
+++ CustomDrivers/Bicycle_Hardware/Lock.cpp
@@ -58,7 +58,7 @@
     SPI_InitStruct.ClockPolarity = LL_SPI_POLARITY_LOW;
     SPI_InitStruct.ClockPhase = LL_SPI_PHASE_1EDGE;
     SPI_InitStruct.NSS = LL_SPI_NSS_SOFT;
-    SPI_InitStruct.BaudRate = LL_SPI_BAUDRATEPRESCALER_DIV64;
+    SPI_InitStruct.BaudRate = LL_SPI_BAUDRATEPRESCALER_DIV128;
     SPI_InitStruct.BitOrder = LL_SPI_LSB_FIRST;
     SPI_InitStruct.CRCCalculation = LL_SPI_CRCCALCULATION_DISABLE;
     SPI_InitStruct.CRCPoly = 10; // not used
