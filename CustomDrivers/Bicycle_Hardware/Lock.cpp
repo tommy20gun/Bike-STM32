e64708d32b814e0405233612865f300b123ef75d
Tommy <tommy20gun00@gmail.com>
2025-05-08 11:00:29 -0700

    preparing for HAL for Lock.cpp branch

--- CustomDrivers/Bicycle_Hardware/Lock.cpp
+++ CustomDrivers/Bicycle_Hardware/Lock.cpp
@@ -78,6 +78,7 @@
     GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
     GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
     LL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+    LL_GPIO_SetOutputPin(GPIOB,LL_GPIO_PIN_12);
 
     //fardriver
     GPIO_InitStruct.Pin = LL_GPIO_PIN_6;
@@ -141,11 +142,12 @@ void Lock::sendCommandFrame(command cmd){
     chipSelect(true);
     LL_SPI_Enable(SPI3);
     int commandIndex = 0;
-    //volatile uint8_t buff;
+    volatile uint8_t buff;
     while (commandArray[cmd][commandIndex] != 0x69){ //end of command magic number
         while(!LL_SPI_IsActiveFlag_TXE(SPI3));
         transmitReceiveByte(commandArray[cmd][commandIndex]);
         //LL_SPI_TransmitData8(SPI3, commandArray[cmd][commandIndex]);
+        buff = transmitReceiveByte(0x00);
         commandIndex++;
     }
     while(!LL_SPI_IsActiveFlag_TXE(SPI3));
